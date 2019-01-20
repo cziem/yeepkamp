bb1e7aab396e22fca462154a6b7d29cf86e49bbf
Fayvor George <phavorsparks@gmail.com>
2019-01-20 02:09:54 +0100

    setup controllers

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('landing')
-})
+const controller = require('../controllers/index')
+
+router.get('/', controller.home)
 
 module.exports = router
