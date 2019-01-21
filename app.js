56b6685f47b7b27788a90da51f2dea81152fe575
Fayvor George <phavorsparks@gmail.com>
2019-01-21 17:25:21 +0100

    setup mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/yelpkamp"
+
+// connect mongoose
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: true
+})
+  .then(() => console.log('connected to the database...'))
+  .catch(err => console.log(`an error occurred: ${err}`))
+
 
 // Require Routes
 const routes = require('./routes/route')
