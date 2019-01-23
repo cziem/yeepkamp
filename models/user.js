64d5105bd83b5fbae22371f401a182f31a1c72a0
Fayvor George <phavorsparks@gmail.com>
2019-01-23 07:51:17 +0100

    Add the user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose')
+
+
+const user = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+user.plugin(passportLocalMongoose)
+
+const User = mongoose.model('User', user)
+
+module.exports = User
