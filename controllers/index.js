c08cd2e823247185ff4257cfb95771eda4137ba2
Fayvor George <phavorsparks@gmail.com>
2019-02-01 22:01:25 +0100

    add logics for getting back password

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ require('../config/config')
 const passport = require('passport')
 const localStrategy = require('passport-local')
 const NodeGeocoder = require("node-geocoder");
-// const multer = require("multer");
 const cloudinary = require("cloudinary");
+const async = require('async')
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 
 // Option Configs
 const options = {
@@ -335,7 +337,10 @@ module.exports = {
 
   // Handle Signup
   addSignup: (req, res) => {
-    let newUser = new User({ username: req.body.username })
+    const newUser = new User({ 
+      username: req.body.username,
+      email: req.body.email
+    })
     const { password } = req.body
 
     if (req.body.adminCode === process.env.ADMIN_CODE) {
@@ -391,6 +396,139 @@ module.exports = {
         req.flash('error', `Could not find ${username}`)
       })
 
+  },
+
+  // Forgot Password
+  forgotPassword: (req, res) => {
+    res.render('users/forgot')
+  },
+
+  recoverPassword: (req, res, next) => {
+    async.waterfall([
+      (done) => {
+        crypto.randomBytes(20, (err, buf) => {
+          const token = buf.toString('hex');
+          done(err, token);
+        });
+      },
+
+      (token, done) => {
+        User.findOne({ email: req.body.email }, (err, user) => {
+          if (!user) {
+            req.flash('error', 'No account with that email address exists.');
+            return res.redirect('/forgot-password');
+          }
+
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+          user.save((err) => {
+            done(err, token, user);
+          });
+        });
+      },
+
+      (token, user, done) => {
+        const smtpTransport = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: 'techampinc@gmail.com',
+            pass: process.env.GMAILPW
+          }
+        });
+
+        const mailOptions = {
+          to: user.email,
+          from: 'techampinc@gmail.com',
+          subject: 'YeepKamp Password Reset',
+          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+        };
+
+        smtpTransport.sendMail(mailOptions, (err) => {
+          console.log('mail sent');
+          req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+          done(err, 'done');
+        });
+      }
+    ], (err) => {
+      if (err) return next(err);
+      res.redirect('/forgot-password');
+    });
+  },
+
+  // Render form for updating password
+  resetPasswordForm: (req, res) => {
+    User.findOne({ 
+      resetPasswordToken: req.params.token, 
+      resetPasswordExpires: { $gt: Date.now() } 
+    }, (err, user) => {
+      if (!user) {
+        req.flash('error', 'Password reset token is invalid or has expired.');
+        return res.redirect('/forgot-password');
+      }
+      res.render('users/reset', { token: req.params.token });
+    });
+  },
+
+  // Handle forgot password request
+  resetPassword: (req, res, next) => {
+    async.waterfall([
+      (done) => {
+        User.findOne({ 
+          resetPasswordToken: req.params.token, 
+          resetPasswordExpires: { $gt: Date.now() } 
+        }, 
+         (err, user) => {
+          if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('back');
+          }
+
+          if (req.body.password === req.body.confirm) {
+            user.setPassword(req.body.password, (err) => {
+              user.resetPasswordToken = undefined;
+              user.resetPasswordExpires = undefined;
+
+              user.save((err) => {
+                req.logIn(user, (err) => {
+                  done(err, user);
+                });
+              });
+            })
+          } else {
+            req.flash("error", "Passwords do not match.");
+            return res.redirect('back');
+          }
+        });
+      },
+      (user, done) => {
+        const smtpTransport = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: 'techampinc@gmail.com',
+            pass: process.env.GMAILPW
+          }
+        });
+
+        const mailOptions = {
+          to: user.email,
+          from: 'techampinc@mail.com',
+          subject: 'Your password has been changed',
+          text: 'Hello,\n\n' +
+            'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+        };
+
+        smtpTransport.sendMail(mailOptions, (err) => {
+          req.flash('success', 'Success! Your password has been changed.');
+          done(err);
+        });
+      }
+    ], (err) => {
+      res.redirect('/campgrounds');
+    });
   }
 }
 
